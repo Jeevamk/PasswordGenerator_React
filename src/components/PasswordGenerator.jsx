f04da680d748efa774ed6782107f882742fc8f4d
Jeeva Mk <138573828+Jeevamk@users.noreply.github.com>
2024-05-13 19:49:51 +0530

    Update PasswordGenerator.jsx

--- src/components/PasswordGenerator.jsx
+++ src/components/PasswordGenerator.jsx
@@ -1,5 +1,4 @@
-import React, { useCallback, useEffect, useRef } from 'react'
-import { useState } from 'react'
+import React, { useCallback, useEffect, useRef ,useState } from 'react'
 
 const PasswordGenerator = () => {
     const [password, setPassword] = useState('')
